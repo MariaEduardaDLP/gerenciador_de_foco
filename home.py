1dcf54b251105f362f444c01c2737027eba4b0f6
MariaEduardaDLP <167711309+MariaEduardaDLP@users.noreply.github.com>
2026-08-29 17:17:03 -0300

    feat: adição de funções básicas para adicionar e excluir tarefas na tela inicial

---
+++ home.py
@@ -0,0 +1,79 @@
+import streamlit as st
+from google.cloud import firestore
+
+BANCO_DADOS = firestore.Client.from_service_account_json("firebase.json")
+
+def pegar_tarefas():
+
+  tarefas = []
+
+  for tarefa in BANCO_DADOS.collection("tarefas").stream():
+    tarefa_quadro = tarefa.to_dict()
+    tarefas.append(tarefa_quadro)
+  
+  return tarefas
+
+def recarregar_tarefas():
+
+  tarefas = pegar_tarefas()
+
+  if not tarefas:
+    st.info(body="Não há tarefas adicionadas")
+  else:
+    st.dataframe(data=tarefas, column_config={ 
+      "status": "Status",
+      "nome": "Nome",
+      "id": None 
+    })
+
+def excluir_tarefas(lista_tarefas):
+
+  for tarefa in lista_tarefas:
+    BANCO_DADOS.collection("tarefas").document(tarefa["id"]).delete()
+
+def main():
+
+  with st.form("nova_tarefa"):
+
+    col1, col2, col3 = st.columns([4, 2, 1])
+
+    with col1:
+      nova_tarefa = st.text_input(label="Nova tarefa", icon=":material/list_alt_add:", placeholder="Digite a sua tarefa do dia...")
+
+    with col2:
+      status = st.selectbox(label="Andamento da tarefa", options=("Não iniciada", "Concluída", "Em progresso"))
+
+    with col3:
+      enviado = st.form_submit_button(label="Adicionar", width="stretch")
+
+    if enviado:
+      tarefa_criada = BANCO_DADOS.collection("tarefas").document()
+      tarefa_criada.set(
+        {
+          "id": tarefa_criada.id,
+          "nome": nova_tarefa,
+          "status": status
+        }
+      )
+
+      st.success(body="Tarefa criada!")
+
+  with st.form("excluir_tarefas"):
+
+    col1, col2 = st.columns([4, 1])
+
+    with col1:
+      tarefas_selecionadas = st.multiselect(label="Excluir tarefas", options=pegar_tarefas(), format_func=lambda tarefa: tarefa["nome"])
+
+    with col2:
+      excluir = st.form_submit_button(label="Excluir", width="stretch")
+
+    if not tarefas_selecionadas and excluir:
+      st.warning(body="Nenhuma tarefa selecionada")
+    elif excluir:
+      excluir_tarefas(tarefas_selecionadas)
+      st.success(body="Tarefas excluídas!")
+
+  recarregar_tarefas()
+
+main()
